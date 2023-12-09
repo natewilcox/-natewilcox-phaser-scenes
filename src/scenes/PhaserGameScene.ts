a60f2413d1f16480b089fc6e261844ffc6ebfec5
natewilcox@gmail.com <natewilcox@gmail.com>
2023-12-08 23:01:34 -0500

    adding resizing methods

--- src/scenes/PhaserGameScene.ts
+++ src/scenes/PhaserGameScene.ts
@@ -9,72 +9,22 @@ export enum GameEvents {
     OnStopFullScreen = 'onstopfullscreen'
 }
 
-export class PhaserGameScene extends Phaser.Scene
-{
-    private DEFAULT_MAX_HEIGHT = 600;
-    private DEFAULT_MAX_WIDTH = 800;
-
-    get isMobile() {
-        return 'contacts' in navigator;
-    }
-
-    get gameWidth() {
-        return Math.min(window.innerWidth, this.DEFAULT_MAX_WIDTH);
-    }
-
-    get gameHeight() {
-        return Math.min(window.innerHeight, this.DEFAULT_MAX_HEIGHT);;
-    }
-
-    create() {
-
-        window.onresize = () => {
-
-            console.log(`onresize [${window.innerWidth}, ${window.innerHeight}]`);
-            this.scale.resize(this.gameWidth, this.gameHeight);
-            this.resizeGameToDevice();
-        }
-
-        //configure hooks resizing when device changes.
-        this.scale.on('resize', () => {
-
-            console.log(`resize [${window.innerWidth}, ${window.innerHeight}]`)
-            this.resizeGameToDevice();
-        }, this);
-
-        this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
-
-            console.log("scene is shutting down")
-            GAME_EVENT_EMMITER.removeAllListeners();
-            this.scale.off('resize')
-        });
-
-        this.scale.resize(this.gameWidth, this.gameHeight);
-    }
-
-    resizeGameToDevice() {
-
-        GAME_EVENT_EMMITER.emit(GameEvents.OnResizing);
-
-        //fill device window if on mobile, or full screen
-        const mediaQuery = window.matchMedia("(max-width: 768px)");  
-        const fullDeviceSize = this.scale.isFullscreen || mediaQuery.matches;
-
-        let width = this.gameWidth;
-        let height = this.gameHeight;
+export class PhaserGameScene extends Phaser.Scene {
+ 
+    setResolution(width: number, height: number) {
 
         //set screen resolution
         this.game.canvas.width = width;
         this.game.canvas.height = height;
 
-        console.log(`resizing to ${width}x${height}`);
+        console.info(`resizing to ${width}x${height}`);
+
         this.game.renderer.resize(width, height);
         this.cameras.main.setSize(width, height);
         this.cameras.main.setViewport(0, 0, width, height);
 
-        setTimeout(() => {
-            GAME_EVENT_EMMITER.emit(GameEvents.OnResized);
-        }, 10)
+        this.scale.resize(width, height);
+        this.scale.refresh();
     }
 
     toggleFullscreen = () => {
